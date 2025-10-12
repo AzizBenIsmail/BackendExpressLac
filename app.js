672d0303e4dac1a466f277a2786616cf79dc8a77
AzizBenIsmail <aziz.270700@gmail.com>
2025-10-12 12:00:27 +0100

    clean Project

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const http = require('http'); //1 importation du module http
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -38,4 +37,12 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+const server = http.createServer(app); //2 creation du serveur
+
+//
+//mongodb+srv://aziz270700_db_aziz:2N3WVF1jgupQ3bXI@cluster0.lgz0ccx.mongodb.net/
+
+//3 le serveur ecoute sur le port 5000
+server.listen(5000, () => {
+  console.log('Server is running on port 5000');
+});
